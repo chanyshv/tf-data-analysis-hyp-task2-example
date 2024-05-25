b60f10031f20f3b30f297787ad622f9ef26044c6
chanyshev <chanyshv@gmail.com>
2024-05-25 20:11:24 +0300

    update

--- solution.py
+++ solution.py
@@ -1,44 +1,59 @@
-import pandas as pd
 import numpy as np
+from scipy.stats import anderson_ksamp
 
 chat_id = 506238275  # Ваш chat ID, не меняйте название переменной
 
 
 def solution(x: np.array, y: np.array) -> bool:
-    x_bins, bins = np.histogram(x, bins=20)
-    y_bins, _ = np.histogram(y, bins)
-    phi = np.sum((y_bins - x_bins) ** 2 / x_bins)
-    df = len(x_bins) - 1
-    # chi2_th = chi2.ppf(1 - 0.1, df)
-    chi2_th = 27.203571029356826
-    return phi > chi2_th
+    result = anderson_ksamp([x, y])
+    return result.significance_level < 0.08
 
 
-def test_solution():
-    historical_data = pd.read_csv('pb2_data/historical_data.csv')
-    modified_data_1 = pd.read_csv('pb2_data/modified_data_of_type_1.csv')
-    modified_data_2 = pd.read_csv('pb2_data/modified_data_of_type_2.csv')
-    modified_data_3 = pd.read_csv('pb2_data/modified_data_of_type_3.csv')
-
-    results = []
-
-    for i in range(len(historical_data)):
-        x = historical_data.iloc[i].values
-        y1 = modified_data_1.iloc[i].values
-        y2 = modified_data_2.iloc[i].values
-        y3 = modified_data_3.iloc[i].values
-
-        result_1 = solution(x, y1)
-        result_2 = solution(x, y2)
-        result_3 = solution(x, y3)
-
-        results.append((result_1, result_2, result_3))
-
-    for i, (res1, res2, res3) in enumerate(results):
-        print(f"Row {i + 1}:")
-        print(f"  Result for modified_data_of_type_1: {res1}")
-        print(f"  Result for modified_data_of_type_2: {res2}")
-        print(f"  Result for modified_data_of_type_3: {res3}")
-
-
-test_solution()
+# def test_solution():
+#     historical_data = pd.read_csv('pb2_data/historical_data.csv')
+#     modified_data_1 = pd.read_csv('pb2_data/modified_data_of_type_1.csv')
+#     modified_data_2 = pd.read_csv('pb2_data/modified_data_of_type_2.csv')
+#     modified_data_3 = pd.read_csv('pb2_data/modified_data_of_type_3.csv')
+#
+#     r1 = 0
+#     r2 = 0
+#     r3 = 0
+#     for i in range(len(historical_data)):
+#         x = historical_data.iloc[i].values
+#         y1 = modified_data_1.iloc[i].values
+#         y2 = modified_data_2.iloc[i].values
+#         y3 = modified_data_3.iloc[i].values
+#
+#         result_1 = solution(x, y1)
+#         result_2 = solution(x, y2)
+#         result_3 = solution(x, y3)
+#
+#         if not result_1: r1 += 1
+#         if not result_2: r2 += 1
+#         if not result_3: r3 += 1
+#
+#     for i, r in enumerate([r1, r2, r3]):
+#         print(f'{i + 1}: {r / 300}')
+#
+#
+# def test_historical_data_pairs():
+#     historical_data = pd.read_csv('pb2_data/historical_data.csv')
+#
+#     pair_results = []
+#     pair_error_count = 0
+#
+#     for i in range(len(historical_data)):
+#         for j in range(i + 1, len(historical_data)):
+#             x = historical_data.iloc[i].values
+#             y = historical_data.iloc[j].values
+#
+#             result = solution(x, y)
+#             pair_results.append(result)
+#
+#             if result: pair_error_count += 1
+#
+#     print(f"Error frequency in historical data pairs: {pair_error_count / len(pair_results)}")
+#
+#
+# test_solution()
+# test_historical_data_pairs()
